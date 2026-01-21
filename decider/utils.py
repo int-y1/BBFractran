57aba1fa43ff309f57bdeca0f58dfca684b1f57f
int-y1 <jason_yuen2007@hotmail.com>
2026-01-20 21:14:08 -0800

    standardize names in utils.py

--- decider/utils.py
+++ decider/utils.py
@@ -3,7 +3,7 @@ from sympy import factorint, prime, primepi
 
 def parse_line(li: str) -> list[list[int]]:
     """
-    :param li: An FM in the format '[x/x, x/x, x/x]' where each 'x' is a positive integer.
+    :param li: An FM in the format `[x/x, x/x, x/x]` where each `x` is a positive integer.
     :return: An FM in vector representation. All vectors have the same length.
     """
     # extract list of numbers
@@ -11,75 +11,75 @@ def parse_line(li: str) -> list[list[int]]:
     assert li.count(']') == 1, "can't find end of fm"
     li = li[li.index('[')+1:li.index(']')]
     assert len(li) > 0, "fm can't be empty"
-    prog: list[tuple[int, int]] = []
+    fm: list[tuple[int, int]] = []
     for f in li.split(','):
-        i, j = f.strip().split('/')
-        prog.append((int(i), int(j)))
+        f1, f2 = f.strip().split('/')
+        fm.append((int(f1), int(f2)))
     # convert to vector representation
-    prog2: list[list[int]] = []
-    maxidx = 0
-    for i, j in prog:
-        f: list[int] = []
-        for p, cnt in factorint(i).items():
+    F: list[list[int]] = []
+    I = 0
+    for f1, f2 in fm:
+        inst: list[int] = []
+        for p, e in factorint(f1).items():
             p = primepi(p)-1
-            maxidx = max(maxidx, p)
-            while len(f) <= maxidx:
-                f.append(0)
-            f[p] += cnt
-        for p, cnt in factorint(j).items():
+            I = max(I, p+1)
+            while len(inst) < I:
+                inst.append(0)
+            inst[p] += e
+        for p, e in factorint(f2).items():
             p = primepi(p)-1
-            maxidx = max(maxidx, p)
-            while len(f) <= maxidx:
-                f.append(0)
-            f[p] -= cnt
-        prog2.append(f)
-    for f in prog2:
-        while len(f) <= maxidx:
-            f.append(0)
-        assert len(f) == maxidx+1  # this should always succeed
-    return prog2
+            I = max(I, p+1)
+            while len(inst) < I:
+                inst.append(0)
+            inst[p] -= e
+        F.append(inst)
+    for inst in F:
+        while len(inst) < I:
+            inst.append(0)
+        assert len(inst) == I  # this should always succeed
+    return F
 
 
 def parse_file(file: str) -> list[list[list[int]]]:
     """
-    :param file: A path to a file. The file should contain FMs parseable by parse_line.
+    :param file: A path to a file. The file should contain FMs parseable by `parse_line`.
     :return: A list of FMs in vector representation.
     """
-    progs = []
+    Fs = []
     with open(file) as f:
         for li in f.read().split('\n'):
             if li.count('[') != 1 or li.count(']') != 1:
                 continue
-            progs.append(parse_line(li))
-    return progs
+            Fs.append(parse_line(li))
+    return Fs
 
 
-def unparse_line(prog: list[list[int]], format: int = 0) -> str:
+def unparse_line(F: list[list[int]], format: int = 0) -> str:
     """
-    The inverse of parse_line.
+    The inverse of `parse_line`.
 
-    :param prog: An FM in vector representation.
+    :param F: An FM in vector representation.
     :param format:
-        0 is the format '[x/x, x/x, x/x]' where each 'x' is a positive integer.
+        0 is the format `[x/x, x/x, x/x]` where each `x` is a positive integer.
         1 is vector representation, as a pretty-printed string.
         2 is vector representation, in C++ format.
     :return: An FM as a string.
     """
     if format == 0:
-        out = []
-        for f in prog:
+        fm = []
+        for inst in F:
             f1 = 1
             f2 = 1
-            for i, e in enumerate(f):
+            for i, e in enumerate(inst):
                 if e > 0:
                     f1 *= prime(i+1)**e
                 if e < 0:
                     f2 *= prime(i+1)**(-e)
-            out.append(f'{f1}/{f2}')
-        return '['+', '.join(out)+']'
+            fm.append(f'{f1}/{f2}')
+        return '['+', '.join(fm)+']'
     elif format == 1:
-        return '\n'.join(' '.join(map(lambda i: f'{i: 2d}', f)) for f in prog)
+        return '\n'.join(' '.join(map(lambda i: f'{i: 2d}', inst)) for inst in F)
     elif format == 2:
-        return '{{'+'},{'.join(','.join(map(str, f)) for f in prog)+'}}'
+        return '{{'+'},{'.join(','.join(map(str, inst)) for inst in F)+'}}'
     else:
         assert False, 'unknown format'
