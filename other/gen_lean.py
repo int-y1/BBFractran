bbc96c64b8c6a4dc94a27086f460859279d4746b
int-y1 <jason_yuen2007@hotmail.com>
2026-03-25 21:02:11 -0700

    modify gen_lean to generate a summary file

--- other/gen_lean.py
+++ other/gen_lean.py
@@ -1,4 +1,5 @@
 # python -m other.gen_lean
+import os
 from decider.utils import parse_file, unparse_line
 
 
@@ -26,7 +27,7 @@ Vector representation:
 {unparse_line(F, 1)}
 ```
 
-TODO: After finishing the proof, replace this paragraph with one of:
+TODO: After the proof compiles without any errors, replace this paragraph with one of:
 * This Fractran program doesn't halt.
 * This Fractran program halts.
 
@@ -47,15 +48,45 @@ theorem nonhalt : ¬halts fm c₀ := by
     return filename, proof_template
 
 
-holdout_file = input('Enter holdouts list (e.g. "sz21_345"): ').strip()
-holdout_number = int(
-    input('Enter holdout number (e.g. from 1 to 345) to generate, or 0 to generate all holdouts: '))
+def fm_to_summary(holdout_directory, holdout_file, holdout_number, F):
+    I = len(F[0])
 
+    # build the fm match definition
+    fm_match = []
+    for inst in F:
+        s0 = ', '.join(chr(ord('a')+i) +
+                       (f'+{-inst[i]}' if inst[i] < 0 else '') for i in range(I))
+        s1 = ', '.join(chr(ord('a')+i) +
+                       (f'+{inst[i]}' if inst[i] > 0 else '') for i in range(I))
+        fm_match.append(f'⟨{s0}⟩ => some ⟨{s1}⟩')
+
+    return f'''import BBfLean.{holdout_directory}.{holdout_file.capitalize()}_{holdout_number}
+''', f'''
+theorem nonhalt{holdout_number} : ¬halts (Q := {' × '.join('ℕ' for _ in range(I))}) (fun q ↦ match q with
+  | {(chr(10)+'  | ').join(fm_match)}
+  | _ => none) ⟨{', '.join('1' if i == 0 else '0' for i in range(I))}⟩ := {holdout_file.capitalize()}_{holdout_number}.nonhalt
+'''
+
+
+holdout_directory = input('Enter holdouts directory (e.g. "Size21"): ').strip()
+os.makedirs(f'other/lean/{holdout_directory}', exist_ok=True)
+holdout_file = input('Enter holdouts list (e.g. "sz21_345"): ').strip()
 holdouts = parse_file(f'holdout/{holdout_file}.txt')
+holdout_min, holdout_max = map(int, input(
+    f'Enter min and max holdout numbers to generate (e.g. enter "1 {len(holdouts)}" to generate all): ').split())
+
+summary1 = []
+summary2 = []
 
 for Fi, F in enumerate(holdouts):
-    if holdout_number > 0 and Fi != holdout_number-1:
+    if not (holdout_min <= Fi+1 <= holdout_max):
         continue
     filename, proof_template = fm_to_lean_file(holdout_file, Fi+1, F)
-    with open(f'other/lean/{filename}', 'w', encoding='utf-8') as f:
+    with open(f'other/lean/{holdout_directory}/{filename}', 'w', encoding='utf-8') as f:
         f.write(proof_template)
+    s1, s2 = fm_to_summary(holdout_directory, holdout_file, Fi+1, F)
+    summary1.append(s1)
+    summary2.append(s2)
+
+with open(f'other/lean/{holdout_directory}Summary.lean', 'w', encoding='utf-8') as f:
+    f.write(''.join(summary1+summary2))
